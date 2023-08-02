9588f797adb978d3ff176afc41f4dc13a42593d1
Mohammed Saeed <105430093+SA33ED@users.noreply.github.com>
2023-08-02 11:34:09 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const BMI());
+}
+
+class BMI extends StatelessWidget {
+  const BMI({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
