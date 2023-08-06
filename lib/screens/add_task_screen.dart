a873b22e3da15581155fa950c0c8d5ed9e8ffd88
Mohammed Saeed <105430093+SA33ED@users.noreply.github.com>
2023-08-07 01:02:21 +0300

    Add Task Form

--- lib/screens/add_task_screen.dart
+++ lib/screens/add_task_screen.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:to_do_app/helpers/app_colors.dart';
+import 'package:to_do_app/widgets/custom_input_field.dart';
 
-class AddTaskScreen extends StatelessWidget {
+class AddTaskScreen extends StatefulWidget {
   const AddTaskScreen({super.key});
   static String id = "AddTaskScreen";
+
+  @override
+  State<AddTaskScreen> createState() => _AddTaskScreenState();
+}
+
+class _AddTaskScreenState extends State<AddTaskScreen> {
+  DateTime? selectedDate = DateTime.now();
+
   @override
   Widget build(BuildContext context) {
     TextTheme customTextTheme = Theme.of(context).textTheme;
@@ -23,6 +33,39 @@ class AddTaskScreen extends StatelessWidget {
         title: Text(
           "Add Task",
           style: customTextTheme.displayLarge,
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(24),
+        child: Form(
+          child: ListView(
+            children: [
+              CustomTextField(title: "Title", hint: "Enter title here"),
+              const SizedBox(height: 20),
+              CustomTextField(title: "Note", hint: "Enter note here"),
+              const SizedBox(height: 20),
+              CustomTextField(
+                title: "Date",
+                hint: DateFormat.yMd().format(selectedDate!),
+                suffixIcon: IconButton(
+                  onPressed: () async {
+                    DateTime? pickedDate = await showDatePicker(
+                      context: context,
+                      initialDate: selectedDate!,
+                      firstDate: DateTime.now(),
+                      lastDate: DateTime(2025),
+                    );
+                    setState(() {
+                      selectedDate = pickedDate;
+                    });
+                  },
+                  icon: const Icon(Icons.calendar_month_sharp),
+                ),
+                readOnly: true,
+              ),
+              const SizedBox(height: 20),
+            ],
+          ),
         ),
       ),
     );
---
+++ lib/widgets/custom_input_field.dart
@@ -0,0 +1,77 @@
+// ignore_for_file: must_be_immutable
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:to_do_app/helpers/app_colors.dart';
+
+class CustomTextField extends StatelessWidget {
+  CustomTextField({
+    super.key,
+    required this.title,
+    this.hint,
+    this.label,
+    this.textInputType,
+    this.suffixIcon,
+    this.obscureText,
+    this.readOnly,
+    this.onChanged,
+    this.validator,
+    this.controller,
+  });
+  String title;
+  bool? obscureText;
+  bool? readOnly;
+  String? hint;
+  Widget? label;
+  Widget? suffixIcon;
+  void Function(String)? onChanged;
+  String? Function(String?)? validator;
+  TextInputType? textInputType;
+  TextEditingController? controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(bottom: 10),
+              child: Text(
+                title,
+                style: GoogleFonts.lato(
+                  color: Colors.white.withOpacity(0.8700000047683716),
+                  fontSize: 16,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            )
+          ],
+        ),
+        TextFormField(
+          controller: controller,
+          validator: validator,
+          decoration: InputDecoration(
+            hintStyle: Theme.of(context).textTheme.displayMedium,
+            fillColor: const Color(0xFF1C1C1C),
+            filled: true,
+            enabledBorder: const OutlineInputBorder(
+              borderSide: BorderSide(color: AppColors.whiteText),
+            ),
+            focusedBorder: const OutlineInputBorder(
+              borderSide: BorderSide(color: AppColors.whiteText),
+            ),
+            hintText: hint,
+            label: label,
+            suffixIconColor: Colors.white,
+            suffixIcon: suffixIcon,
+          ),
+          keyboardType: textInputType,
+          onChanged: onChanged,
+          obscureText: obscureText ?? false,
+          readOnly: readOnly ?? false,
+        )
+      ],
+    );
+  }
+}
