ce8273e22189696a590aaed1cbfd545541ea4d1c
Mohammed Saeed <105430093+SA33ED@users.noreply.github.com>
2023-08-07 17:50:18 +0300

    Add Dummy Data

--- lib/models/task_model.dart
+++ lib/models/task_model.dart
@@ -1,9 +1,34 @@
-class Task {
-  Task({
+import 'package:flutter/material.dart';
+import 'package:to_do_app/helpers/app_colors.dart';
+
+class TaskModel {
+  TaskModel({
+    required int id,
     required String title,
     required String note,
     required DateTime date,
-    required DateTime startTime,
-    required DateTime endTime,
+    required String startTime,
+    required String endTime,
+    String? status = "TODO",
+    Color? color = AppColors.red,
   });
 }
+
+List<TaskModel> tasks = [
+  TaskModel(
+    id: 1,
+    title: "Task One",
+    note: "Learning Dart",
+    date: DateTime.now(),
+    startTime: "09:33 AM",
+    endTime: "09:45 PM",
+  ),
+  TaskModel(
+    id: 2,
+    title: "Task Two",
+    note: "Learning Flutter",
+    date: DateTime.now(),
+    startTime: "10:33 AM",
+    endTime: "10:45 PM",
+  ),
+];
